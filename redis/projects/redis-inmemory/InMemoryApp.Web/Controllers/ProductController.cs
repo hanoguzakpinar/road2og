9f8f6e3f4c57f3b301366235e7d8be35e11b584c
hanoguzakpinar <hanoguzakpinar@gmail.com>
2025-11-30 18:44:21 +0300

    inmemory-trygetvalue-remove

--- redis/projects/redis-inmemory/InMemoryApp.Web/Controllers/ProductController.cs
+++ redis/projects/redis-inmemory/InMemoryApp.Web/Controllers/ProductController.cs
@@ -14,13 +14,44 @@ namespace InMemoryApp.Web.Controllers
 
         public IActionResult Index()
         {
-            _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            //1.yol cache var mı kontrolü
+            if (String.IsNullOrEmpty(_memoryCache.Get<string>("zaman")))
+            {
+                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            }
+
+            //2.yol cache var mı kontrolü
+            if (_memoryCache.TryGetValue("zaman", out string? zamanCache))
+            {
+                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            }
+
             return View();
         }
 
         public IActionResult Show()
         {
             ViewBag.Zaman = _memoryCache.Get<string>("zaman");
+
+            return View();
+        }
+
+        public IActionResult Remove()
+        {
+            //cacheden silme
+            _memoryCache.Remove("zaman");
+
+            return View();
+        }
+
+        public IActionResult GetOrCreate()
+        {
+            //cache'den okumaya çalışır, yoksa oluşturur
+            _memoryCache.GetOrCreate<string>("zaman", c =>
+            {
+                //c.AbsoluteExpiration = DateTimeOffset.Now.AddMinutes(5);
+                return DateTime.Now.ToString();
+            });
 
             return View();
         }
