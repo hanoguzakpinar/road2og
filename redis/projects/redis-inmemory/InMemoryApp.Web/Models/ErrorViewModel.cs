15570d62587b77dd15c82a12669c11472018b040
hanoguzakpinar <hanoguzakpinar@gmail.com>
2025-11-30 11:56:45 +0300

    inmemory-cache-part1

---
+++ redis/projects/redis-inmemory/InMemoryApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace InMemoryApp.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
