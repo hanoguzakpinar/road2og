8ec516f9a704e3d2ed65c61df719c5e09e2c48d2
hanoguzakpinar <hanoguzakpinar@gmail.com>
2025-12-09 20:36:42 +0300

    redis.API-initial

---
+++ redis/projects/RedisExample.API/Models/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RedisExample.API.Models;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+    {
+
+    }
+
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>().HasData(
+            new Product { Id = 1, Name = "Kalem", Price = 10 },
+            new Product { Id = 2, Name = "Macbook", Price = 10000 },
+            new Product { Id = 3, Name = "Playstation", Price = 5000 }
+        );
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ redis/projects/RedisExample.API/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using RedisExample.API.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseInMemoryDatabase("myDatabase");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
