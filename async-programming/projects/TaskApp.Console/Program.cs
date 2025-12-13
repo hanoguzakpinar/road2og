ad5f927e65de420e2ecb13ebaf683eff0e079d5d
hanoguzakpinar <hanoguzakpinar@gmail.com>
2025-12-13 21:03:51 +0300

    task.ContinueWith

---
+++ async-programming/projects/TaskApp.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿System.Console.WriteLine("task.ContinueWith");
+
+// var task = new HttpClient().GetStringAsync("https://www.google.com").ContinueWith((data) =>
+// {//data Task<string> türünde olduğu için sonuç data.Result propertysinden alınır
+//     Console.WriteLine($"uzunluk: {data.Result.Length}");
+// });
+
+var task = new HttpClient().GetStringAsync("https://www.google.com").ContinueWith(print);
+
+System.Console.WriteLine("arada yapılacak işler");
+
+await task;
+
+static void print(Task<string> task)
+{
+    //task Task<string> türünde olduğu için sonuç data.Result propertysinden alınır
+    Console.WriteLine($"uzunluk: {task.Result.Length}");
+}
