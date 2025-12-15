53d75cbe7d69c906fc2265fbdc151f825b93692c
hanoguzakpinar <hanoguzakpinar@gmail.com>
2025-12-15 20:00:33 +0300

    task.Run-2

--- async-programming/projects/TaskApp.Console/Run.cs
+++ async-programming/projects/TaskApp.Console/Run.cs
@@ -5,4 +5,9 @@
 
  //Task.Run'ın birincil amacı, CPU Yoğunluklu (CPU-bound) görevleri (uzun süren hesaplamalar, döngüler, veri işleme) ana iş parçacığından (özellikle kullanıcı arayüzü veya sunucu istek iş parçacığı) ayırarak uygulamanın yanıt verebilirliğini korumaktır.
 
- 
+ //Task.Run, genellikle eşzamanlı (synchronous) çalışan kod bloklarını alıp onları asenkron bir bağlamda çalıştırmak için bir köprü görevi görür.
+
+ /* Task.Run'ın İki Yaygın Kullanım Senaryosu
+  1. Senkron Metotları Asenkron Yapmak: Mevcut bir kütüphanedeki veya eski koddaki asenkron alternatifi olmayan senkron bir metodu (örneğin bir dosya okuma metodu) asenkron bir bağlamda çalıştırmak için.
+  2. Aşırı CPU Yükünü Ayırmak: Bir web sunucusu (API) veya masaüstü uygulamasının Ana İş Parçacığından yoğun hesaplama gerektiren görevleri ayırmak için kullanılır.
+ */
