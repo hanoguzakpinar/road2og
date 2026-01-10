0855bafe53c0ff9001848069efc362e77bc28fe5
hanoguzakpinar <hanoguzakpinar@gmail.com>
2026-01-10 13:44:16 +0300

    rabbitmq.2

---
+++ rabbitmq/projects/RabbitMq.Publisher/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory()
+{
+    Uri = new Uri("amqps://hcjxfkdh:U7T630lKSQsPOHo3DnL1H0aG9-Zq1Mqw@collie.lmq.cloudamqp.com/hcjxfkdh")
+};
+
+using var connection = await factory.CreateConnectionAsync();
+
+var channel = await connection.CreateChannelAsync();
+
+// durable = RabbitMQ sunucusu yeniden başlatılsa (restart) bile kuyruğun veya exchange'in kaybolmayıp fiziksel olarak diskte saklanmasını sağlar.
+// exclusive = Kuyruğun sadece onu oluşturan bağlantı (connection) tarafından kullanılabilmesini ve o bağlantı kapandığında kuyruğun otomatik olarak silinmesini sağlar.
+// autoDelete = Kuyruğu dinleyen son tüketici (consumer) aboneliğini (unsubscribe) bitirdiğinde kuyruğun otomatik olarak silinmesini sağlar.
+await channel.QueueDeclareAsync("hello-queue", true, false, false);
+
+string msg = "hello world";
+var body = Encoding.UTF8.GetBytes(msg);
+
+await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "hello-queue", basicProperties: new BasicProperties(), body: body, mandatory: false);
+
+System.Console.WriteLine("Mesaj gönderilmiştir.");
+Console.ReadLine();
---
+++ rabbitmq/projects/RabbitMq.Subscriber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory()
+{
+    Uri = new Uri("amqps://hcjxfkdh:U7T630lKSQsPOHo3DnL1H0aG9-Zq1Mqw@collie.lmq.cloudamqp.com/hcjxfkdh")
+};
+
+using var connection = await factory.CreateConnectionAsync();
+
+var channel = await connection.CreateChannelAsync();
+
+//kuyruk declare edilmezse ve bu kuyruk yoksa hata fırlatılır.
+//publisher'ın bu kuyruğu oluşturduğuna dair kesinlik varsa bu satır silinebilir.
+//zaten kuyruk varsa ve bu satır çalışırsa, hata oluşmaz.
+//publisher tarafında ve consumer tarafında aynı parametreler ile kuyruk oluşturulmalıdır. yoksa uygulama hata verir.
+//await channel.QueueDeclareAsync("hello-queue", true, false, false);
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+
+//autoAck = Mesaj tüketiciye ulaştığı anda, tüketici tarafındaki işlemin bitmesi veya hata alması beklenmeden mesajın başarılı sayılmasını ve kuyruktan hemen silinmesini sağlar.
+await channel.BasicConsumeAsync("hello-queue", autoAck: true, consumer);
+
+consumer.ReceivedAsync += async (sender, ea) =>
+{
+    var msg = Encoding.UTF8.GetString(ea.Body.ToArray());
+
+    System.Console.WriteLine($"Gelen Mesaj: {msg}");
+};
+
+
+Console.ReadLine();
