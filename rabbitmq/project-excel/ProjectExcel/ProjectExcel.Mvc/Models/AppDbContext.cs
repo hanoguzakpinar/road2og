5c1bfb1e2bff7ac2cd819502ec299aa4448d6d43
Oğuzhan Akpınar <oguzhan.akpinar@belbim.istanbul>
2026-02-14 13:12:19 +0300

    rabbitmq.project-Excel.initial

---
+++ rabbitmq/project-excel/ProjectExcel/ProjectExcel.Mvc/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjectExcel.Mvc.Models
+{
+	public class AppDbContext : IdentityDbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+		{
+
+		}
+
+        public DbSet<UserFile> UserFiles { get; set; }
+    }
+}
---
+++ rabbitmq/project-excel/ProjectExcel/ProjectExcel.Mvc/Program.cs
@@ -0,0 +1,68 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ProjectExcel.Mvc.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+	opt.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
+});
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(opt =>
+{
+	opt.User.RequireUniqueEmail = true;
+}).AddEntityFrameworkStores<AppDbContext>();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+	//di'dan db alma
+	var appDbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+	var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+	appDbContext.Database.Migrate();
+
+	if (!appDbContext.Users.Any())
+	{
+		userManager.CreateAsync(new IdentityUser
+		{
+			UserName = "admin",
+			Email = "admin@gmail.com"
+		}, "password").Wait();
+		userManager.CreateAsync(new IdentityUser
+		{
+			UserName = "deneme",
+			Email = "deneme@gmail.com"
+		}, "password").Wait();
+	}
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}")
+	.WithStaticAssets();
+
+
+app.Run();
