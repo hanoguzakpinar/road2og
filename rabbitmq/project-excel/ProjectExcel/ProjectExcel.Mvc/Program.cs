30ff6646efaa0c8bba7adabc5661b14d119975e4
hanoguzakpinar <hanoguzakpinar@gmail.com>
2026-02-25 20:47:30 +0300

    rabbitmq.project-Excel rabbitmqClientService eklendi

--- rabbitmq/project-excel/ProjectExcel/ProjectExcel.Mvc/Program.cs
+++ rabbitmq/project-excel/ProjectExcel/ProjectExcel.Mvc/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using ProjectExcel.Mvc.Models;
+using ProjectExcel.Mvc.Services;
+using RabbitMQ.Client;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,46 +11,55 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
-	opt.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
 });
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>(opt =>
 {
-	opt.User.RequireUniqueEmail = true;
+    opt.User.RequireUniqueEmail = true;
 }).AddEntityFrameworkStores<AppDbContext>();
+
+builder.Services.AddSingleton(sp =>
+{
+    return new ConnectionFactory()
+    {
+        Uri = new Uri(builder.Configuration.GetConnectionString("RabbitMq"))
+    };
+});
+builder.Services.AddSingleton<RabbitMQClientService>();
 
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
 {
-	//di'dan db alma
-	var appDbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    //di'dan db alma
+    var appDbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
 
-	var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
 
-	appDbContext.Database.Migrate();
+    appDbContext.Database.Migrate();
 
-	if (!appDbContext.Users.Any())
-	{
-		userManager.CreateAsync(new IdentityUser
-		{
-			UserName = "admin",
-			Email = "admin@gmail.com"
-		}, "Password*5").Wait();
-		userManager.CreateAsync(new IdentityUser
-		{
-			UserName = "deneme",
-			Email = "deneme@gmail.com"
-		}, "Password*5").Wait();
-	}
+    if (!appDbContext.Users.Any())
+    {
+        userManager.CreateAsync(new IdentityUser
+        {
+            UserName = "admin",
+            Email = "admin@gmail.com"
+        }, "Password*5").Wait();
+        userManager.CreateAsync(new IdentityUser
+        {
+            UserName = "deneme",
+            Email = "deneme@gmail.com"
+        }, "Password*5").Wait();
+    }
 }
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Home/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -60,9 +71,9 @@ app.UseAuthorization();
 app.MapStaticAssets();
 
 app.MapControllerRoute(
-	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}")
-	.WithStaticAssets();
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
 
 
 app.Run();
---
+++ rabbitmq/project-excel/ProjectExcel/ProjectExcel.Mvc/Services/RabbitMQClientService.cs
@@ -0,0 +1,57 @@
+﻿using RabbitMQ.Client;
+
+namespace ProjectExcel.Mvc.Services
+{
+    public class RabbitMQClientService : IAsyncDisposable
+    {
+        private readonly ConnectionFactory _connectionFactory;
+        private IConnection _connection;
+        private IChannel _channel;
+        public static string ExchangeName = "ExcelDirectExchange";
+        public static string RoutingExcel = "excel-route-file";
+        public static string QueueName = "queue-excel-file";
+        private readonly ILogger<RabbitMQClientService> _logger;
+        public RabbitMQClientService(ConnectionFactory connectionFactory, ILogger<RabbitMQClientService> logger)
+        {
+            _connectionFactory = connectionFactory;
+            _logger = logger;
+        }
+
+        public async Task<IChannel> ConnectAsync()
+        {
+            _connection = await _connectionFactory.CreateConnectionAsync();
+
+            if (_channel is { IsOpen: true })
+                return _channel;
+
+            _channel = await _connection.CreateChannelAsync();
+
+            await _channel.ExchangeDeclareAsync(ExchangeName, ExchangeType.Direct, true, false);
+
+            await _channel.QueueDeclareAsync(QueueName, true, false, false, null);
+
+            await _channel.QueueBindAsync(QueueName, ExchangeName, RoutingExcel);
+
+            _logger.LogInformation("RabbitMq ile bağlantı kuruldu.");
+
+            return _channel;
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (_channel != null)
+            {
+                await _channel.CloseAsync();
+                await _channel.DisposeAsync();
+            }
+
+            if (_connection != null)
+            {
+                await _connection.CloseAsync();
+                await _connection.DisposeAsync();
+            }
+
+            _logger.LogInformation("RabbitMq ile bağlantı kapatıldı.");
+        }
+    }
+}
