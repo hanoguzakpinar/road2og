045f76e49bd2fa8a7f786a2a8603a3637f5dd52f
Oğuzhan Akpınar <hanoguzakpinar@gmail.com>
2026-02-26 11:10:52 +0300

    rabbitmq.project-Excel / files controller eklendi

---
+++ rabbitmq/project-excel/ProjectExcel/ProjectExcel.Mvc/Controllers/FilesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectExcel.Mvc.Models;
+
+namespace ProjectExcel.Mvc.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class FilesController : ControllerBase
+	{
+		private readonly AppDbContext _context;
+
+		public FilesController(AppDbContext context)
+		{
+			_context = context;
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Upload(IFormFile file, int fileId)
+		{
+			if (file is not { Length: > 0 }) return BadRequest();
+
+			var userFile = await _context.UserFiles.FirstAsync(x => x.Id == fileId);
+
+			var filePath = userFile.FileName + Path.GetExtension(file.FileName);
+
+			var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "files", filePath);
+
+			using (var stream = new FileStream(path, FileMode.Create))
+			{
+				await file.CopyToAsync(stream);
+			}
+
+			userFile.CreatedDate = DateTime.Now;
+			userFile.FilePath = filePath;
+			userFile.FileStatus = FileStatus.Completed;
+
+			await _context.SaveChangesAsync();
+
+			return Ok();
+		}
+	}
+}
