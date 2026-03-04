e8488684e7c061c779ae91341253e6909b6a6938
Oğuzhan Akpınar <hanoguzakpinar@gmail.com>
2026-03-04 14:32:30 +0300

    docker.Console - part 2

--- docker/DockerProjects/DockerConsole/Program.cs
+++ docker/DockerProjects/DockerConsole/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-for (int i = 0; i < 50; i++)
+for (int i = 1; i < 50; i++)
 {
-	Console.WriteLine("Hello, World!");
+	Console.WriteLine($"Hello, World! {i}");
+
+	await Task.Delay(5000);
 }
